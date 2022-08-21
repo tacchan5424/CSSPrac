ab14ed20f80b72114d60bb60514f240ea7ebfc55
tacchan5424s <tacchan5424@yahoo.co.jp>
2022-08-21 12:12:59 +0900

    コメント整理

--- class/twinbee.js
+++ class/twinbee.js
@@ -3,7 +3,7 @@ import { CharactorBase } from "./base/charactorBase.js";
 export class Twinbee extends CharactorBase {
   constructor() {
     super();
-    this._positionX = 500;
+    this._positionX = 925;
     this._positionY = 500;
     this._directionX = 0;
     this._directionY = 0;
