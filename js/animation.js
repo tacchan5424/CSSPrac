43233c0810239fe0c9183d74b6d70afb313ed099
tacchan5424s <tacchan5424@yahoo.co.jp>
2022-08-29 23:38:59 +0900

    リファクタリングとヒット時のエフェクト、ディレイ実装

---
+++ js/animation.js
@@ -0,0 +1,24 @@
+export const flash = {
+  process: [
+    {
+      opacity: 1,
+      offset: 0,
+    },
+    {
+      opacity: 0,
+      offset: 0.2,
+    },
+    {
+      opacity: 1,
+      offset: 0.4,
+    },
+    {
+      opacity: 1,
+      offset: 1,
+    },
+  ],
+  setting: {
+    duration: 500,
+    animationIterationCount: 2,
+  },
+};
